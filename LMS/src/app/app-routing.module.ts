35c90f73640bfccf110d066edf6c3fade7880708
Dev_Khatab <khatap1@hotmail.com>
2021-04-06 04:45:16 +0200

    Add Routes

--- LMS/src/app/app-routing.module.ts
+++ LMS/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
+import { NewLoanComponent } from './loan/new-loan/new-loan.component';
+import { LoanComponent } from './loan/loan.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+{ path: '', component:  LoanComponent },
+{ path: 'loan/new', component: NewLoanComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
